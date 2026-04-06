a71f50ca8204d0b553e477ef4db56eae900b6287
altafin <altafin@uel.br>
2026-04-06 17:51:43 -0300

    Criado layout do formulário com livewire para cadastro de post.

---
+++ resources/views/pages/post/⚡create.blade.php
@@ -0,0 +1,46 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public string $title = '';
+
+    public string $content = '';
+
+    public function save()
+    {
+        $this->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        dd($this->title, $this->content);
+    }
+};
+?>
+
+<section class="w-full">
+{{--    @include('partials.settings-heading')--}}
+    <flux:heading class="sr-only">{{ __('Profile settings') }}</flux:heading>
+    <x-pages::settings.layout :heading="__('Post')" :subheading="__('cadastro de Post')">
+        <form wire:submit="save" class="my-6 w-full space-y-6">
+            <flux:input wire:model="title" :label="__('Title')" type="text" required autofocus autocomplete="title" />
+
+            <div>
+                <flux:textarea wire:model="content" :label="__('Content')" type="textarea" required autofocus autocomplete="content" />
+            </div>
+
+            <div class="flex items-center gap-4">
+                <div class="flex items-center justify-end">
+                    <flux:button variant="primary" type="submit" class="w-full" data-test="update-profile-button">
+                        {{ __('Save Post') }}
+                    </flux:button>
+                </div>
+                <x-action-message class="me-3" on="post-updated">
+                    {{ __('Saved.') }}
+                </x-action-message>
+            </div>
+        </form>
+    </x-pages::settings.layout>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('/post/create', 'pages::post.create')->name('post.create');
 });
 
 require __DIR__.'/settings.php';
