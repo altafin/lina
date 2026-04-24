c54d53c95718ca9e47d459823e2e1d5ee3d6418e
altafin <marbr72@hotmail.com>
2026-04-23 22:44:32 -0300

    Lista de pessoas cadastradas

--- resources/views/pages/person/⚡index.blade.php
+++ resources/views/pages/person/⚡index.blade.php
@@ -2,10 +2,19 @@
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Computed;
+use App\Models\Person;
+use Livewire\WithPagination;
 
 new #[Title('People')] class extends Component
 {
-    //
+    use WithPagination;
+
+    #[Computed]
+    public function people()
+    {
+        return Person::latest()->paginate(5);
+    }
 };
 ?>
 
@@ -19,4 +28,56 @@ new #[Title('People')] class extends Component
     </flux:modal.trigger>
 
     <livewire:person.create/>
+
+    {{-- Table --}}
+    <div class="overflow-x-auto">
+        <table class="w-full text-sm">
+            <thead class="bg-zinc-100 dark:bg-zinc-700 border-b border-zinc-200 dark:border-zinc-800">
+            <tr>
+                <th class="px-6 py-4 text-left">Name</th>
+                <th class="px-6 py-4 text-left">Type</th>
+                <th class="px-6 py-4 text-right">Actions</th>
+            </tr>
+            </thead>
+
+            <tbody class="divide-y divide-zinc-200 dark:divide-zinc-800 bg-white dark:bg-zinc-950">
+            @forelse($this->people as $person)
+                <tr wire:key="person-{{ $person->id }}" class="hover:bg-zinc-50 dark:hover:bg-zinc-900/50 transition">
+                    <td class="px-6 py-4 font-medium text-zinc-900 dark:text-white">
+                        {{ $person->name }}
+                    </td>
+
+                    <td class="px-6 py-4 text-zinc-600 dark:text-zinc-200 truncate max-w-md">
+                        {{ $person->type_id }}
+                    </td>
+
+                    <td class="px-6 py-4">
+                        <div class="flex justify-end gap-2">
+                            <flux:button
+                                variant="primary"
+                                color="amber"
+                                wire:click="$dispatch('edit-person', {id: {{ $person->id }}})">
+                                Edit
+                            </flux:button>
+
+                            <flux:button
+                                variant="danger"
+                                wire:click="$dispatch('confirm-delete', {id: {{ $person->id }}})">
+                                Delete
+                            </flux:button>
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4" class="px-6 py-4 text-center text-zinc-500 dark:text-zinc-400">
+                        No people found.
+                    </td>
+
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
+    </div>
+
 </div>
